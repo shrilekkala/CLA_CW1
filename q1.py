79332f40f99cc0d8fc2d08fe06731d284e1e2677
Shri Lekkala <shri.lekkala@gmail.com>
2020-11-11 23:20:17 +0530

    Investigating diagonal components of R and condition number of A

--- q1.py
+++ q1.py
@@ -1,5 +1,6 @@
 from numpy import *
 from cla_utils import *
+import matplotlib.pyplot
 
 A = load('values.npy')
 
@@ -9,3 +10,10 @@ Q,R =  GS_modified(A)
 # investigate the orthogonality of Q
 err = Q.T @ Q - np.eye(Q.shape[1])
 print(np.linalg.norm(err))
+
+# investigate the diagonal components of R
+matplotlib.pyplot.semilogy(R.diagonal(0))
+matplotlib.pyplot.show()
+
+# check the condition number of A
+print(np.linalg.cond(A))
