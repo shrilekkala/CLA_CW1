0c0d95594f38e614b23d9c91701f4765aa2f45ee
Shri Lekkala <shri.lekkala@gmail.com>
2020-11-13 00:47:36 +0530

    Constructed Linear system, Formed a Least Squares Function

--- q2.py
+++ q2.py
@@ -1,6 +1,42 @@
 from numpy import *
 from cla_utils import *
+from scipy import linalg_solve_triangular
+from matplotlib import pyplot as plt 
 
 x = arange(-1,1.1,0.2)
 f = 0.0*x
 f[3:6] = 1
+
+# construct a linear system given m
+def linear_system(m):
+
+    # construct matrix A
+    A = np.ones((11,m))
+    for i in range (1,m):
+        A[:,i] = x**i
+
+    # construct vector b
+    b = f.copy()
+
+    return(A,b)
+
+# Solve the least squares system using the QR method
+def least_squares(A,b):
+
+    # obtain Q and R by modified Gram-Schmidt
+    Q, R = GS_modified(A)
+    
+    # solve for x using back substitution
+    x_ls = scipy.linalg.solve_triangular(R, Q.T @ b)
+
+    return x
+
+A,b = linear_system(10)
+coef = least_squares(A,b)
+f_ls = A @ coef
+
+# plot of original data
+plt.plot(x,f) 
+
+# plot of the resulting polynomial using least squares
+plt.plot(x,f_ls) 
