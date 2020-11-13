cc4bab6ebbf5c582e6491259a14b2412de308074
Shri Lekkala <shri.lekkala@gmail.com>
2020-11-13 17:41:55 +0530

    Adjusted plots and added comparison to built-in QR

--- q3.py
+++ q3.py
@@ -14,15 +14,14 @@ def Vand(p):
 def plot_col_q(Q):
 
     for i in range(5):
-        plt.plot(x, Q[:,i], label = "Col." + str(i+1))
+        plt.plot(x, Q[:,-5+i], label = "Col." + str(i+1))
 
-    plt.legend(fontsize=8, loc='upper left')
     plt.xlabel("Column of Q")
     plt.ylabel("x")
 
 
 # create the required x vector
-M = 100
+M = 500
 x = np.arange(-M + 0.5, M + 0.5) / M
 
 
@@ -31,29 +30,37 @@ Q, R = householder_qr(Vand(p))
 
 plt.title("Plot of Data x against Columns of Q (Householder)")
 plot_col_q(Q)
+plt.legend(fontsize=8, loc='upper left')
 plt.show()
 
 
 # Invesigate QR factorisation for a large p
-p = 100
+p = 150
 
 # Householder
 Qh, Rh = householder_qr(Vand(p))
 
-plt.title("Plot of Data x against the last 5 Columns of Q (Householder)")
+plt.title("Plot of Data x against the last 5 Columns of Q (Householder), p = " + str(p))
 plot_col_q(Qh)
 plt.show()
 
 # Classical Gram-Schmidt
 Qc, Rc = GS_classical(Vand(p))
 
-plt.title("Plot of Data x against the last 5 Columns of Q (Classical GS)")
+plt.title("Plot of Data x against the last 5 Columns of Q (Classical GS), p = " + str(p))
 plot_col_q(Qc)
 plt.show()
 
 # Modified Gram-Schmidt
 Qm, Rm = GS_modified(Vand(p))
 
-plt.title("Plot of Data x against the last 5 Columns of Q (Modified GS)")
+plt.title("Plot of Data x against the last 5 Columns of Q (Modified GS), p = " + str(p))
 plot_col_q(Qm)
+plt.show()
+
+# Built in
+Qb, Rb = np.linalg.qr(Vand(p))
+
+plt.title("Plot of Data x against the last 5 Columns of Q (Built in QR), p = " + str(p))
+plot_col_q(Qb)
 plt.show()
