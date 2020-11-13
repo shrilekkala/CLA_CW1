5feeaf5053c82518477f4fa10c5b0c1d1af371d8
Shri Lekkala <shri.lekkala@gmail.com>
2020-11-13 13:47:01 +0530

    Plotted and compared columns of Q for different QR methods

--- q3.py
+++ q3.py
@@ -2,23 +2,58 @@ import numpy as np
 from cla_utils import *
 from matplotlib import pyplot as plt 
 
+# function that generates the required Vandermonde matrix given p
+def Vand(p):
+
+    V = np.vander(x, p)
+    V = np.flip(V, axis = 1)
+
+    return V
+
+# function that generates the plot of x against the last 5 columns of Q
+def plot_col_q(Q):
+
+    for i in range(5):
+        plt.plot(x, Q[:,i], label = "Col." + str(i+1))
+
+    plt.legend(fontsize=8, loc='upper left')
+    plt.xlabel("Column of Q")
+    plt.ylabel("x")
+
+
+# create the required x vector
 M = 100
 x = np.arange(-M + 0.5, M + 0.5) / M
+
+
 p = 5
+Q, R = householder_qr(Vand(p))
 
-# generate the required Vandermonde matrix
-V = np.vander(x, p)
-V = np.flip(V, axis = 1)
-
-Q, R = householder_qr(V)
-
-for i in range(5):
-    plt.title("Data against Column " + str(i) + " of Q")
-    plt.plot(x, Q[:,i])
+plt.title("Plot of Data x against Columns of Q (Householder)")
+plot_col_q(Q)
 plt.show()
 
-for i in range(5):
-    plt.title("Column " + str(i) + " of Q against the Data")
-    plt.plot(Q[:,i], x)
+
+# Invesigate QR factorisation for a large p
+p = 100
+
+# Householder
+Qh, Rh = householder_qr(Vand(p))
+
+plt.title("Plot of Data x against the last 5 Columns of Q (Householder)")
+plot_col_q(Qh)
 plt.show()
 
+# Classical Gram-Schmidt
+Qc, Rc = GS_classical(Vand(p))
+
+plt.title("Plot of Data x against the last 5 Columns of Q (Classical GS)")
+plot_col_q(Qc)
+plt.show()
+
+# Modified Gram-Schmidt
+Qm, Rm = GS_modified(Vand(p))
+
+plt.title("Plot of Data x against the last 5 Columns of Q (Modified GS)")
+plot_col_q(Qm)
+plt.show()
