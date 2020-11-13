7ac3370ff04bd35ec895f0831058c3a9e5dcbf1a
Shri Lekkala <shri.lekkala@gmail.com>
2020-11-13 12:57:53 +0530

    Updated test script for q3 to verify the Householder QR

--- test_q3.py
+++ test_q3.py
@@ -1,1 +1,26 @@
-### test script for q3 ####
+''' test script for q3 '''
+import pytest
+import cla_utils
+import q3
+
+from numpy import random
+import numpy as np
+
+# import variables from q1.py
+Q = q3.Q
+R = q3.R
+V = q3.V
+
+### test the accuracy of the householder QR factorisation ###
+def test_QR_accuracy():
+    err = V - Q@R
+    assert(np.linalg.norm(err) < 1.0e-6)
+
+### test if the householder Q is orthogonal ###
+def test_QR_orthogonality():
+    err = np.eye(Q.shape[1]) - Q.T @ Q
+    assert(np.linalg.norm(err) < 1.0e-6)
+
+### test if the householder R is upper triangular ###
+def test_R_triangular():
+    assert(np.allclose(np.triu(R), R, 1.0e-6))
