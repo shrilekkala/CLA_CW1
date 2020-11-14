1235fba2668f1df8607cbd8744f26853f3306d0b
Shri Lekkala <shri.lekkala@gmail.com>
2020-11-14 16:25:26 +0530

    Updated after small error was spotted in observed data

--- q4.py
+++ q4.py
@@ -14,7 +14,7 @@ def Vand(x, p):
 
     return V
 
-p = 10
+p = 4
 
 # formulate matrices A, B and vectors b, d from the data
 A = np.zeros((100,2*(p+1)))
@@ -24,12 +24,12 @@ A[51:,p+1:] = Vand(data[51:,0], p+1)
 B = np.zeros((2,2*(p+1)))
 B[0, :p+1] = np.array([1 * data[50,0]**i for i in range(p+1)])
 B[0, p+1:] = - B[0, :p+1]
-B[1, :p+1] = np.multiply(np.array([1 * data[50,0]**(i-1) for i in range(p+1)]), np.arange(p+1))
+B[1, :p+1] = np.multiply(np.array([1 * data[49,0]**(i-1) for i in range(p+1)]), np.arange(p+1))
 B[1, p+1:] = - np.multiply(np.array([1 * data[51,0]**(i-1) for i in range(p+1)]), np.arange(p+1))
 
 b = data[:,1]
 
-d = np.array([0, -5]).T
+d = np.array([0, 0]).T
 
 
 # obtain the full QR of B^T
@@ -54,12 +54,20 @@ x = Qb @ np.vstack((y1[:, np.newaxis], y2[:, np.newaxis]))
 plt.scatter(data[:,0] ,data[:,1], c="r", marker='x', label = "Observed Data") 
 
 # generate evenly distributed values from 0 to 2
-x_vals = np.arange(0,2.02,0.02)
-y_vals = x_vals.copy()
-y_vals[:51][:, np.newaxis] = Vand(x_vals[:51], p+1) @ x[:p+1]
-y_vals[50:][:, np.newaxis] = Vand(x_vals[50:], p+1) @ x[p+1:]
-plt.plot(x_vals[:51], y_vals[:51])
-plt.plot(x_vals[50:], y_vals[50:])
+"""
+x_vals = np.arange(0,2.02,0.01)
+y_vals = x_vals.copy()[:, np.newaxis]
+y_vals[:101] = Vand(x_vals[:101], p+1) @ x[:p+1]
+y_vals[100:] = Vand(x_vals[100:], p+1) @ x[p+1:]
+plt.plot(x_vals[:101], y_vals[:101])
+plt.plot(x_vals[99:], y_vals[99:])
+"""
+
+plt.plot(data[:,0], A@x)
+plt.show()
+
 
 print(np.linalg.norm(A@x - b))
-plt.show()
+
+RSS = np.sum ((A @ x - data[:,1]) ** 2)
+print(np.sqrt(RSS))
