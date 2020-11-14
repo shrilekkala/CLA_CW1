13bb75cf8cb031a7611bb7ffc4c475afb7c3ffa8
Shri Lekkala <shri.lekkala@gmail.com>
2020-11-15 00:46:07 +0530

    Arranged question into functions, tested various values of p for part c)

--- q4.py
+++ q4.py
@@ -14,60 +14,73 @@ def Vand(x, p):
 
     return V
 
-p = 4
+def generate_mats(p):
+    # formulate matrices A, B and vectors b, d from the data
+    A = np.zeros((100,2*(p+1)))
+    A[:51,:p+1] = Vand(data[:51,0], p+1)
+    A[51:,p+1:] = Vand(data[51:,0], p+1)
 
-# formulate matrices A, B and vectors b, d from the data
-A = np.zeros((100,2*(p+1)))
-A[:51,:p+1] = Vand(data[:51,0], p+1)
-A[51:,p+1:] = Vand(data[51:,0], p+1)
+    B = np.zeros((2,2*(p+1)))
+    B[0, :p+1] = np.array([1 * data[50,0]**i for i in range(p+1)])
+    B[0, p+1:] = - B[0, :p+1]
+    B[1, :p+1] = np.multiply(np.array([1 * data[50,0]**(i-1) for i in range(p+1)]), np.arange(p+1))
+    B[1, p+1:] = - np.multiply(np.array([1 * data[50,0]**(i-1) for i in range(p+1)]), np.arange(p+1))
 
-B = np.zeros((2,2*(p+1)))
-B[0, :p+1] = np.array([1 * data[50,0]**i for i in range(p+1)])
-B[0, p+1:] = - B[0, :p+1]
-B[1, :p+1] = np.multiply(np.array([1 * data[49,0]**(i-1) for i in range(p+1)]), np.arange(p+1))
-B[1, p+1:] = - np.multiply(np.array([1 * data[51,0]**(i-1) for i in range(p+1)]), np.arange(p+1))
+    b = data[:,1]
 
-b = data[:,1]
-
-d = np.array([0, 0]).T
+    d = np.array([0,-5]).T
+    return A, B, b, d
 
 
-# obtain the full QR of B^T
-Qb, Rb = householder_full_qr(B.T)
-Rb = Rb[:2,:2]
+# function that generates the piecwise least squares polynomial
+def piecewise_poly(p):
+    A, B, b, d = generate_mats(p)
 
-# solve for y_1 by back substitution
-y1 = scipy.linalg.solve_triangular(Rb, d)
+    # obtain the full QR of B^T
+    Qb, Rb = householder_full_qr(B.T)
+    Rb = Rb[:2,:2]
 
-A1 = A @ Qb[:, :2]
-A2 = A @ Qb[:, 2:]
+    # solve for y_1 by back substitution
+    y1 = scipy.linalg.solve_triangular(Rb.T, d)
 
-# obtain the reduced QR of A2
-Qa2, Ra2 = householder_qr(A2)
+    A1 = A @ Qb[:, :2]
+    A2 = A @ Qb[:, 2:]
 
-# solve the least squares model for y_2
-y2 = scipy.linalg.solve_triangular(Ra2, Qa2.T @ (b - A1 @ y1))
+    # obtain the reduced QR of A2
+    Qa2, Ra2 = householder_qr(A2)
 
-# substitute y1 and y2 back to get x
-x = Qb @ np.vstack((y1[:, np.newaxis], y2[:, np.newaxis]))
+    # solve the least squares model for y_2
+    y2 = scipy.linalg.solve_triangular(Ra2, Qa2.T @ (b - A1 @ y1))
+
+    # substitute y1 and y2 back to get x
+    x = Qb @ np.vstack((y1[:, np.newaxis], y2[:, np.newaxis]))
+
+    return x
 
 plt.scatter(data[:,0] ,data[:,1], c="r", marker='x', label = "Observed Data") 
 
 # generate evenly distributed values from 0 to 2
-"""
 x_vals = np.arange(0,2.02,0.01)
+
+p = 10
+x = piecewise_poly(p)
+
 y_vals = x_vals.copy()[:, np.newaxis]
 y_vals[:101] = Vand(x_vals[:101], p+1) @ x[:p+1]
-y_vals[100:] = Vand(x_vals[100:], p+1) @ x[p+1:]
+y_vals[101:] = Vand(x_vals[101:], p+1) @ x[p+1:]
 plt.plot(x_vals[:101], y_vals[:101])
-plt.plot(x_vals[99:], y_vals[99:])
-"""
+plt.plot(x_vals[100:], y_vals[100:])
 
-plt.plot(data[:,0], A@x)
 plt.show()
 
 
-print(np.linalg.norm(A@x - b))
+RSS_mat = np.zeros(20)
 
-RSS = np.sum ((A @ x - data[:,1]) ** 2)
-print(np.sqrt(RSS))
+for i in range(1,21):
+    A = generate_mats(i)[0]
+    x = piecewise_poly(i)
+    RSS = np.sum ((A @ x - data[:,1][:, np.newaxis]) ** 2)
+    RSS_mat[i-1] = RSS
+
+plt.semilogy(range(1,21), RSS_mat)
+plt.show()
