8bced8cf68182e0bfc6313c83483303a8f50d7ea
Shri Lekkala <shri.lekkala@gmail.com>
2020-11-15 20:18:01 +0530

    Created appropriate matrices for part d

--- q4.py
+++ q4.py
@@ -71,7 +71,7 @@ y_vals[101:] = Vand(x_vals[101:], p+1) @ x[p+1:]
 plt.plot(x_vals[:101], y_vals[:101])
 plt.plot(x_vals[100:], y_vals[100:])
 
-plt.show()
+plt.show() ###################################################
 
 
 RSS_mat = np.zeros(20)
@@ -83,4 +83,70 @@ for i in range(1,21):
     RSS_mat[i-1] = RSS
 
 plt.semilogy(range(1,21), RSS_mat)
-plt.show()
+plt.show() ###################################################
+
+
+"""
+---------
+PART D 
+---------
+"""
+# generates a random parametric ellipse
+def ellipse_with_noise(n, a, b):
+    # generate random thetas and order them
+    theta_i = np.sort(np.random.uniform(0,2*np.pi,(n)))
+
+    # generate an ellipse with random noise using the theta values
+    x_i = 5 + a * np.cos(theta_i) + np.random.multivariate_normal(np.zeros(n),np.eye(n)/5)
+    y_i = 3 + b * np.sin(theta_i) + np.random.multivariate_normal(np.zeros(n),np.eye(n)/5)
+        
+    return theta_i, x_i, y_i
+
+theta_i, x_i, y_i = ellipse_with_noise(100,20,10)
+
+plt.scatter(x_i,y_i, c="r", marker='x', label = "Ellipse") 
+plt.show() ###################################################
+
+# Fix M and p
+M = 10
+p = 5
+
+A_mats = list()
+for m in range(M):
+    # Obtain the index of theta values that are between 2*m*pi/M and 2*(m+1)*pi/M
+    index = np.where(np.logical_and(theta_i>=2*np.pi*m/M, theta_i<2*np.pi*(m+1)/M))
+
+    print(index)
+    # Generate Vandermonde matrix for C_m
+    A_mats.append(Vand(theta_i[index], p+1))
+    
+
+# generate block diagonal matrix A
+A = A_mats[0]
+for i in range(1,M):
+    A = scipy.linalg.block_diag(A, A_mats[i])
+    
+# generate matrix B
+B = np.zeros((2*M,M*(p+1)))
+for i in range(M-1):
+    # continuity
+    B[i,i*(p+1):(i+1)*(p+1)] = np.array([1 * (2*np.pi*(i+1) / M)**i for i in range(p+1)])
+    B[i,(i+1)*(p+1):(i+2)*(p+1)] = - B[i,i*(p+1):(i+1)*(p+1)]
+                                            
+    # continuity of derivatives
+    B[i+M,i*(p+1):(i+1)*(p+1)] = np.multiply(np.array([1 * (2*np.pi*(i+1) / M)**(i-1) for i in range(p+1)]), np.arange(p+1))
+    B[i+M,(i+1)*(p+1):(i+2)*(p+1)] = - B[i+M,i*(p+1):(i+1)*(p+1)]
+
+# edge case
+B[M-1,(M-1)*(p+1):M*(p+1)] = np.array([1 * (2*np.pi)**i for i in range(p+1)])
+B[M-1, 0] = -1
+
+B[2*M-1,(M-1)*(p+1):M*(p+1)] = np.multiply(np.array([1 * (2*np.pi)**(i-1) for i in range(p+1)]), np.arange(p+1))
+B[2*M-1, 1] = -1
+
+# generate matrix b
+b = np.concatenate((x_i[:, np.newaxis],y_i[:, np.newaxis]), axis=1)
+
+# generate matrix d
+d = np.zeros((2*M, 2))
+    
