c03c33cdb4940d94c0cbf6a32493e6bc2b0c8b5a
Shri Lekkala <shri.lekkala@gmail.com>
2020-11-18 15:24:53 +0530

    Cleaned up the files, arranged functions, added docstrings, added question numbers

--- q1.py
+++ q1.py
@@ -4,16 +4,34 @@ from matplotlib import pyplot as plt
 
 A = np.load('values.npy')
 
-# compute the QR factorisation of A using the modified Gram-Schmidt algorithm
+""" Question 1a) """
+
+# compute the QR factorisation of A using the modified Gram-Schmidt algorithm from 
 Q,R =  GS_modified(A)
+
+""" Question 1b) """
 
 # investigate the orthogonality of Q
 err = Q.T @ Q - np.eye(Q.shape[1])
-print(np.linalg.norm(err))
+print("The error in the orthogonality of Q is", np.linalg.norm(err))
 
 # investigate the diagonal components of R
 plt.semilogy(R.diagonal(0))
+plt.title("Semilog plot of the diagonal components of R")
+plt.xlabel("n")
+plt.ylabel("n'th component of diagonal of R")
 plt.show()
 
+# plot the columns of R
+plt.title("Plot of the columns of R")
+for i in range(100):
+    plt.plot(R[:,i])
+plt.xlabel("n")
+plt.ylabel("n'th element of a column of R")
+plt.show()
+
+""" Question 1c) """
+
 # check the condition number of A
-print(np.linalg.cond(A))
+print("The condition number of A is", np.linalg.cond(A))
+
--- q2.py
+++ q2.py
@@ -7,18 +7,27 @@ x = np.arange(-1,1.1,0.2)
 f = 0.0*x
 f[3:6] = 1
 
-# construct the linear system given m, x
-def linear_system(m, x):
+def Vandermonde(m, x):
+    """
+    Given a polynomial degree m and an array pf points x,
+    construct the Vandermonde matrix V for use in the linear system Vx = b
 
-    # construct matrix A
-    A = np.ones((11,m+1))
+    :return A: the matrix A in the linear system
+    """
+    # construct Vandermonde Matrix A
+    A = np.ones((x.shape[0],m+1))
     for i in range (1,m+1):
         A[:,i] = x**i
 
     return(A)
 
-# Solve the least squares system using the QR method
 def least_squares(A,b):
+    """
+    Given a matrix A and a vector b,
+    solve the least squares system to Ax = b using the QR method
+ 
+    :return x: the least squares solution to Ax = b
+    """
 
     # obtain Q and R by modified Gram-Schmidt
     Q, R = GS_modified(A)
@@ -28,55 +37,44 @@ def least_squares(A,b):
 
     return coefficients
 
-# Construct a Vandermonde matrix so the least squares polynomial curve can be plotted
-def Vandermonde(m):
-
-    V = np.ones((101,m+1))
-    for i in range (1,m+1):
-        V[:,i] = x_vals ** i
-    
-    return V
-
-# generate evenly distributed values from -1 to 1
-x_vals = np.arange(-1,1.02,0.02)
-
-# function generates the plots of the least squares polynomial for the data given an m, x and f
 def plot_ls_poly(m, x, f):
+    """
+    Given a polynomial degree m, array of points fxand a data f,
+    generate the plots of the least squares polynomial for Ax = f
+ 
+    """
 
-    A = linear_system(m,x)
+    A = Vandermonde(m,x)
     coef = least_squares(A,f)
 
     # generate the y values to be plotted using the least squares polynomial
-    y_vals = Vandermonde(m) @ coef
+    y_vals = Vandermonde(m, x_vals) @ coef
 
     # plot of the  polynomial resulting from  least squares
     plt.plot(x_vals,y_vals, zorder=0, label = "Least Squares model")
 
-# generate a small random vector of the same length as f using the multivariate normal distribution
 def pert(f):
-
+    """
+    Given a vector f,
+    perturb f by a small random vector obtained from a multivariate normal distribution
+ 
+    :return df: the randomly perturbed vector
+    """
     df = np.random.multivariate_normal(np.zeros(len(f)),np.eye(len(f))/100)
     return df
 
-
-
-plot_ls_poly(10, x, f)
-plt.title("Least squares polynomial of degree 10 for the data")
-plt.scatter(x,f, c="r", marker='x', label = "Observed Data") 
-plt.legend(loc='best')
-plt.show()
-
-plot_ls_poly(7, x, f)
-plt.scatter(x,f, c="r", marker='x', label = "Observed Data") 
-plt.title("Least squares polynomial of degree 7 for the data")
-plt.legend(loc='best')
-plt.show()
-
 # Investigate the sensitivity of the 2 polynomials above under the small random perturbations
-# for a given m (degree of poly) and n (number of random perturbations to data)
 def pert_sensitivity(m, n):
-
-    A = linear_system(m,x)
+    """
+    Given a polynomial degree m, and a number of times to run n
+    Obtain the coefficients vector to Ax = f using least squares.
+    Perturb f to obtain df and obtain the coefficents vector to Ax = df using least squares.
+    Compute the sum of squared differences between the two coefficients vectors.
+    Repeat this process with a different ranodm perturbation n times.
+ 
+    :return SSD: the array of sum of squared differences for each of the n perturbations
+    """
+    A = Vandermonde(m,x)
     coef = least_squares(A,f)
     SSD = np.zeros(n)
     for i in range(n):
@@ -85,18 +83,51 @@ def pert_sensitivity(m, n):
         SSD[i] = np.linalg.norm (coef - dcoef)
     return SSD
 
+# generate evenly distributed values from -1 to 1 to be used for plotting
+x_vals = np.arange(-1,1.02,0.02)
+
+""" Question 2c) """
+
+# plot the original data
+plt.scatter(x,f, c="r", marker='x', label = "Observed Data") 
+# plot the least squares solution where m = 10
+plot_ls_poly(10, x, f)
+plt.title("Least squares polynomial of degree 10 for the data")
+plt.legend(loc='best')
+plt.show()
+
+""" Question 2d) """
 
 plt.title("Least squares polynomials of degree 10 for the randomly perturbed data")
+# plot the original data
 plt.scatter(x,f, c="r", marker='x', label = "Observed Data") 
+# plot the least squares solution where for each random perturbation
 for i in range(10):
     plot_ls_poly(10, x, f + pert(f))
 plt.show()
 
-plt.title("Least squares polynomials of degree 7 for the randomly perturbed data")
+# print the sum of squared differences for each perturbation
+print(pert_sensitivity(10, 15))
+
+""" Question 2e) """
+
+# plot the original data
 plt.scatter(x,f, c="r", marker='x', label = "Observed Data") 
+# plot the least squares solution where m = 7
+plot_ls_poly(7, x, f)
+plt.title("Least squares polynomial of degree 7 for the data")
+plt.legend(loc='best')
+plt.show()
+
+"""  Question 2f) """
+
+plt.title("Least squares polynomials of degree 7 for the randomly perturbed data")
+# plot the original data
+plt.scatter(x,f, c="r", marker='x', label = "Observed Data") 
+# plot the least squares solution where for each random perturbation
 for i in range(10):
     plot_ls_poly(7, x, f + pert(f))
 plt.show()
 
-print(pert_sensitivity(10, 15))
+# print the sum of squared differences for each perturbation
 print(pert_sensitivity(7, 15))
